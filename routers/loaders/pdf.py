1f1a6ee8d150c32d8b03b0bac6ae7e4539376c52
David Peterson <34923485+Haste171@users.noreply.github.com>
2023-09-24 23:41:42 -0500

    Formatting Fix for PDF loader * add .env example

--- routers/loaders/pdf.py
+++ routers/loaders/pdf.py
@@ -7,19 +7,19 @@ engine = Engine()
 router = APIRouter()
 
 @router.post("/pdf")
-async def file(file: UploadFile, namespace: str):
+async def file(upload_file: UploadFile, namespace: str):
     """
     Loader: https://llamahub.ai/l/file-pymu_pdf
     """
-    file_preview_name, file_extension = os.path.splitext(file.filename)
+    file_preview_name, file_extension = os.path.splitext(upload_file.filename)
     if file_extension != '.pdf':
         raise HTTPException(status_code=400, detail="File must be a PDF")
     
     with tempfile.NamedTemporaryFile(delete=True, prefix=file_preview_name + '_', suffix=".pdf") as temp_file:
-        content = await file.read()
+        content = await upload_file.read()
         temp_file.write(content)
         PyMuPDFReader = download_loader("PyMuPDFReader")
         loader = PyMuPDFReader().load(file_path=temp_file.name, metadata=True)
         engine.load(loader, namespace)
     
-    return {'message': 'File uploaded successfully', 'filename': file.filename, "namespace": namespace}
+    return {'message': 'File uploaded successfully', 'filename': upload_file.filename, "namespace": namespace}
